836cd893844b54c33f83d7996ecbc52fa528e5ae
Daigoreng <daigoren5@gmail.com>
2019-06-21 23:39:39 +0900

    gradleエラーの修正

--- app/src/main/java/com/example/f_lab/testlocation1/FirstActivity.java
+++ app/src/main/java/com/example/f_lab/testlocation1/FirstActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
